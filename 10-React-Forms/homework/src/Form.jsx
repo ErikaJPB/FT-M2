09b20e01171b84c671b71cc7d12183267ec4dffd
ErikaJPB <erikajpinedab@gmail.com>
2022-10-11 12:55:47 -0500

    Homework

--- 10-React-Forms/homework/src/Form.jsx
+++ 10-React-Forms/homework/src/Form.jsx
@@ -1,9 +1,75 @@
-import React from 'react';
+import React from "react";
+import { useState } from "react";
 
-export default function  Form() {
+export const validate = (input) => {
+  let errors = {};
+
+  if (!input.username) {
+    errors.username = "Username is required";
+  } else if (!/\S+@\S+\.\S+/.test(input.username)) {
+    errors.username = "Username is invalid";
+  }
+
+  if (!input.password) {
+    errors.password = "Password is required";
+  } else if (!/(?=.*[0-9])/.test(input.password)) {
+    errors.password = "Password is invalid";
+  }
+
+  return errors;
+};
+
+export default function Form() {
+  const [input, setInput] = useState({
+    username: "",
+    password: "",
+  });
+
+  const [errors, setErrors] = useState({
+    username: "",
+    password: "",
+  });
+
+  const handleInputChange = (event) => {
+    const value = event.target.value;
+    const property = event.target.name;
+    setInput({ ...input, [property]: value });
+    setErrors(validate({ ...input, [property]: value }));
+  };
+
   return (
-      <div>
-        Componente Form
-      </div>
-  )
+    <form>
+        <label>Username:</label>
+        <input
+        className={errors.username && 'danger'}
+          name='username'
+          type='text'
+          onChange={handleInputChange}
+          value={input.username}
+        ></input>
+
+        {errors.username && (<p className='danger'>{errors.username}</p>)} 
+      
+         <hr style= {{borderStyle: 'none'}} />
+
+         <label>Password:</label>
+        <input
+        className={errors.password && 'danger'}
+          name='password'
+          type='password'
+          onChange={handleInputChange}
+          value={input.password}
+        ></input>
+
+        {errors.password && (<p className='danger'>{errors.password}</p>)} 
+      
+      <hr style= {{borderStyle:'none'}} />
+      <button 
+              onClick={handleInputChange}
+              disabled = {errors.username || errors.password}
+              > Submit</button>
+    </form>
+  );
 }
+
+//html for : indica que el label corresponde a ese input
